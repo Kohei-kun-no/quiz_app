0944d4d65352791f0ae9feae8e3d716443b3555e
Kohei-kun-no <k.minoda1205@iCloud.com>
2022-02-21 17:41:48 +0900

    fix ElevatedButton

--- lib/answer.dart
+++ lib/answer.dart
@@ -10,12 +10,17 @@ class Answer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      child: RaisedButton(
-        color: Colors.blue,
-        textColor: Colors.white,
-        child: Text(answerText),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.blue,
+        ),
+        child: Text(answerText, style: TextStyle(color: Colors.white),),
         onPressed: selectHandler,
       ),
     );
   }
 }
+// color: Colors.blue,
+// textColor: Colors.white,
+// child: Text(answerText),
+// onPressed: selectHandler,
